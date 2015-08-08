47979f64b6a1bc64ef16185e9415ddd88b8b7bda
wvxvw <olegsivokon@gmail.com>
2015-08-08 13:39:33 +0300

    Modified drawing code to color the filled hexagons

--- src/render-svg.lisp
+++ src/render-svg.lisp
@@ -3,21 +3,27 @@
 (defparameter *hex-width* 10)
 (defparameter *hex-height* (ceiling (/ *hex-width* (/ (sqrt 3) 2))))
 
-(defun draw-board (name width height)
-  (svg:with-svg-to-file
-      (scene 'svg:svg-1.1-toplevel
-             :width (+ (/ *hex-width* 2) (* *hex-width* width))
-             :height (* *hex-height* height))
-      ((make-pathname :type "svg" :defaults (pathname name)) :if-exists :supersede)
-    (svg:draw scene
-        (:rect :x 0 :y 0
-               :width (* *hex-width* (+ 1 width))
-               :height (* *hex-height* (+ 1 height))
-               :style "fill:rgb(255,255,255)"))
-    (mapcar #'(lambda (col)
-                (mapcar #'(lambda (row) (draw-hex scene col row))
-                        (alexandria:iota height)))
-            (alexandria:iota width))))
+(defun draw-board (name board)
+  (let ((width (array-dimension board 1))
+        (height (array-dimension board 0)))
+    (svg:with-svg-to-file
+        (scene 'svg:svg-1.1-toplevel
+               :width (+ (/ *hex-width* 2) (* *hex-width* width))
+               :height (* *hex-height* height))
+        ((make-pathname :type "svg" :defaults (pathname name)) :if-exists :supersede)
+      (svg:draw scene
+                (:rect :x 0 :y 0
+                       :width (* *hex-width* (+ 1 width))
+                       :height (* *hex-height* (+ 1 height))
+                       :style "fill:rgb(255,255,255)"))
+      (iter
+        (for col :below width)
+        (iter
+          (for row :below height)
+          (draw-hex scene col row
+                    :fill (if (= 1 (aref board row col))
+                              "yellow"
+                              "none")))))))
 
 (defparameter *hex-vertices* (list (cons (/ *hex-width* 2) 0)
                                    (cons *hex-width* (/ *hex-height* 4))
@@ -27,14 +33,14 @@
                                    (cons 0 (/ *hex-height* 4))
                                    (cons (/ *hex-width* 2) 0)))
 
-(defun draw-hex (scene column row)
+(defun draw-hex (scene column row &key (fill "none"))
   (svg:draw scene
             (:polyline :points (coord-list->string
                                 (mapcar
                                  #'(lambda (vertice)
                                      (transpose vertice column row))
                                  *hex-vertices*))
-            :fill "none" :stroke "black")))
+            :fill fill :stroke "black")))
 
 (defun transpose (coord column row)
   (cons
@@ -49,3 +55,9 @@
 
 (defun coord->string (coord)
   (format nil "~d,~d" (car coord) (cdr coord)))
+
+;; testing
+
+(defun test-draw-board (input &optional (output "test"))
+  (init-game input)
+  (draw-board output *board*))
