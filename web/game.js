f6023b3668c104baea6d77d0e00b0fc25d8e643a
wvxvw <olegsivokon@gmail.com>
2015-08-08 16:52:56 +0300

    Needed to remove old content in body.

--- web/game.js
+++ web/game.js
@@ -2,6 +2,7 @@ $(function ($) {
     var body = $('body');
     body.keyup(function (event) {
         $.post('/move.svg', { move: event.key }, function (data) {
+            body.empty();
             body.append(data);
         }).fail(function () {
             alert('error');
