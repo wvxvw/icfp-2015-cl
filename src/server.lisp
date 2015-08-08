124f5a87ffea193f8e6379575ee7e2ace93222ab
wvxvw <olegsivokon@gmail.com>
2015-08-08 19:09:48 +0300

    Can select games and pass directions.

--- src/server.lisp
+++ src/server.lisp
@@ -3,28 +3,27 @@
 (defparameter *server* nil)
 
 (defun game-to-svg (&key (board *board*) (unit *unit*))
-  (unless *board*
-    (init-game (asdf:system-relative-pathname
-                (asdf:find-system :icfp-2015-cl) #p"./problems/problem_3.json")))
-  (let* ((width (array-dimension board 1))
-         (height (array-dimension board 0))
-         (svg (svg:make-svg-toplevel 'svg:svg-1.1-toplevel
-                                     :width (+ (/ *hex-width* 2) (* *hex-width* width))
-                                     :height (* *hex-height* height))))
-    (svg:draw svg
-              (:rect :x 0 :y 0
-                     :width (* *hex-width* (+ 1 width))
-                     :height (* *hex-height* (+ 1 height))
-                     :style "fill:rgb(255,255,255)"))
-    (iter
-      (for col :below width)
-      (iter
-        (for row :below height)
-        (draw-hex svg col row
-                  :fill (if (= 1 (aref board row col))
-                            "yellow"
-                            "none"))))
-    svg))
+  (if (and *board* *unit*)
+      (let* ((width (array-dimension board 1))
+             (height (array-dimension board 0))
+             (svg (svg:make-svg-toplevel 'svg:svg-1.1-toplevel
+                                         :width (+ (/ *hex-width* 2) (* *hex-width* width))
+                                         :height (* *hex-height* height))))
+        (svg:draw svg
+                  (:rect :x 0 :y 0
+                         :width (* *hex-width* (+ 1 width))
+                         :height (* *hex-height* (+ 1 height))
+                         :style "fill:rgb(255,255,255)"))
+        (iter
+          (for col :below width)
+          (iter
+            (for row :below height)
+            (draw-hex svg col row
+                      :fill (if (= 1 (aref board row col))
+                                "yellow"
+                                "none"))))
+        svg)
+      (error "No active game")))
 
 (defun output-svg (svg &optional (stream *standard-output*))
   (svg:stream-out stream svg))
@@ -32,6 +31,19 @@
 (tbnl:define-easy-handler (move :uri "/move.svg"
                                 :default-request-type :post)
     (direction)
+  (tbnl:log-message* :error "~&direction: ~s" direction)
+  (with-output-to-string (stream)
+    (output-svg (game-to-svg) stream)))
+
+(tbnl:define-easy-handler (play :uri "/play.svg"
+                                :default-request-type :post)
+    (game)
+  ;; sorry, I'm too lazy to set up logging.
+  (tbnl:log-message* :error "~&game: ~s" game)
+  (tbnl:log-message* :error "~&params: ~s" (tbnl:post-parameters*))
+  (init-game (asdf:system-relative-pathname
+              (asdf:find-system :icfp-2015-cl)
+              (format nil "./problems/~a" game)))
     (with-output-to-string (stream)
       (output-svg (game-to-svg) stream)))
 
--- web/game.js
+++ web/game.js
@@ -1,6 +1,10 @@
 $(function ($) {
-    var body = $('body'),
-        keys = { "65": "e", "70": "w", "83": "se", "68": "sw", "37": "cw", "39": "ccw" };
+    var body = $('body'), play = $('#play'), container = $('#container'),
+        keys = { '65': 'e', '70': 'w', '83': 'se', '68': 'sw', '37': 'cw', '39': 'ccw' };
+    function loadBoard(data) {
+        container.empty();
+        container.append(data);
+    }
     body.keyup(function (event) {
         console.log('key: ' + event.keyCode);
         // 65 - a
@@ -11,11 +15,15 @@ $(function ($) {
         // 39 - right
         if ([65, 70, 83, 68, 37, 39].indexOf(event.keyCode) > -1) {
             $.post('/move.svg', { move: keys[event.keyCode] }, function (data) {
-                body.empty();
-                body.append(data);
+                loadBoard(data);
             }).fail(function () {
                 alert('error');
             });
         }
+    });
+    play.change(function (event) {
+        console.log('play this game: ' + $('#play option:selected').text());
+        $.post('/play.svg', { game : $('#play option:selected').text() },
+               function (data) { loadBoard(data); });
     });
 });
