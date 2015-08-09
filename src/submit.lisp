a73283c2d567af804024ab592c3734b576a1a1e3
Zach Kost-Smith <zachkostsmith@gmail.com>
2015-08-09 11:07:29 -0500

    Allow commands to be given a simple string

--- src/submit.lisp
+++ src/submit.lisp
@@ -21,15 +21,17 @@
     table))
 
 (defun encode-solution (commands)
-  (with-output-to-string (stream)
-    (iter
-      (for cmd :in commands)
-      (cond ((gethash cmd *translation-table*)
-             (princ (aref (gethash cmd *translation-table*) 0)
-                    stream))
-            ((characterp cmd)
-             (princ cmd stream))
-            (t (error "Garbage in command sequence"))))))
+  (if (stringp commands)
+      commands
+      (with-output-to-string (stream)
+        (iter
+          (for cmd :in commands)
+          (cond ((gethash cmd *translation-table*)
+                 (princ (aref (gethash cmd *translation-table*) 0)
+                        stream))
+                ((characterp cmd)
+                 (princ cmd stream))
+                (t (error "Garbage in command sequence")))))))
 
 (defun solution-to-string (problem-number seed commands &optional (tag "test-submission"))
   (with-output-to-string (out)
